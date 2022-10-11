36f1d82f213036c8b3062956b1e4b2ad52382743
Tirtha Pratim Saha <ranjanmaji906@gmail.com>
2022-10-11 18:16:09 +0530

    css-add

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -4,10 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const Products = () => {
-  const [FontSize , setFontSize] = useState(16);
+  const [FontSize, setFontSize] = useState(16);
   return (
     <>
-    {/* <div onClick={ () =>{
+      {/* <div onClick={ () =>{
       setFontSize(prev => prev + 16)
     }} style={{
 fontSize : FontSize,
@@ -19,14 +19,49 @@ cursor:'pointer'
      className = " jay jay1">
         This is Products Page
     </div> */}
-    <div class="d-flex justify-content-center">
-    <div class="p-2">Flex item 1</div>
-     <div class="p-2">Flex item 2</div>
-     <div class="p-2">Flex item 3</div>
-    </div>
+      <div class="containeritem">
+
+
+
+
+
+        <div class="containerproduct item-1">
+
+          <div class="contanercircle ">
+            <div class="circlegol"></div>
+
+          </div>
+        </div>
+
+
+        <div class="containerproduct item-2">
+
+          <div class="contanercircle ">
+            <div class="circlegol"></div>
+
+          </div>
+        </div>
+
+
+        <div class="containerproduct item-3">
+
+          <div class="contanercircle ">
+            <div class="circlegol"></div>
+
+          </div>
+        </div>
+
+      
+
+
+      </div>
+      <div class= "desc">
+        <h5>React and Node.js are both open-source JavaScript libraries that are used to build user interfaces and server-side applications, respectively. Both are the most popular technologies in the world of front-end web development today, but they’re quite different.</h5>
+        
+      </div>
 
     </>
-  
+
   )
 }
 
